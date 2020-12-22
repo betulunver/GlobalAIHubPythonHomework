f6e8c2ba07a9bb60700e03921bdfcc194321dd32
Betül Ünver <30605842+betulunver@users.noreply.github.com>
2020-12-22 09:45:02 +0300

    Add files via upload

---
+++ Day0.py
@@ -0,0 +1,29 @@
+
+#DAY0 Homework
+
+
+#take 5 values from user and write a program that prints the values you get on the screen.
+value0 = input("Please enter value: ")
+value1 = input("Please enter value: ")
+value2 = input("Please enter value: ")
+value3 = input("Please enter value: ")
+value4 = input("Please enter value: ")
+
+#print the type of values you received in this program on the screen.
+
+data0 = 5
+data1 = 3.9
+data2 = [3, 5, 7]
+print(type(data0))
+print(type(data1))
+print(type(data2))
+
+
+#when using print functions, do not forget to use f-string and format usage in your program.
+
+#f-string
+print(f'Data0  : {data0}')
+
+#.format
+print("Data1 : {}".format(data1))
+
